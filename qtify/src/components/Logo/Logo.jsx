6f3acbec174d84b2f2d1b3ff959ce51235669457
luckyanand15 <luckyanand.741@gmail.com>
2024-10-03 03:11:22 +0530

    Navbar frontend reset

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
-import Styles from "./Logo.module.css";
 
 export default function Logo() {
-  return (
-    <div className={Styles.logo}>
-      <img src={LogoImage} alt="logo" width={67} />
-    </div>
-  );
+  return <img src={LogoImage} alt="logo" width={67} />;
 }
