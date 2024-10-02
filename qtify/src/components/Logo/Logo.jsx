8d32b673ce3b0f353c960af35817f09030316282
luckyanand15 <luckyanand.741@gmail.com>
2024-10-03 03:05:09 +0530

    Nabvar frontend fixed all components

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -3,5 +3,9 @@ import LogoImage from "../../assets/logo.png";
 import Styles from "./Logo.module.css";
 
 export default function Logo() {
-  return <img className={Styles.logo} src={LogoImage} alt="logo" width={67} />;
+  return (
+    <div className={Styles.logo}>
+      <img src={LogoImage} alt="logo" width={67} />
+    </div>
+  );
 }
